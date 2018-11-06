257c475c2b641a9be5583a968f56daaf8b55e31a
Usin2705 <Usin2705@yahoo.com>
2018-11-06 18:11:03 +0200

    feat: add tts and move (in one code)

---
+++ RaspberryPi3/FishSauce.py
@@ -0,0 +1,24 @@
+#import asyncio
+from SubPackage.ttsModule import tts
+from SubPackage.UartCommunication import UartCommunication
+from subprocess import call
+
+isTTS = False
+
+uart = UartCommunication(9600)
+call(["/usr/bin/amixer", "cset", "numid=1", "85%"])
+speech = input("Press T to use text to speech.\nPress M to move.\nType exit to quit.\n")
+
+if speech.lower()=="t":	
+	isTTS = True	
+		
+while (speech.lower()!="exit"):
+	if isTTS:
+		speech = input("Please enter your speech:")
+		if (speech.lower()!="m"):
+			tts(speech)
+		else:
+			isTTS = False
+	else:
+		isTTS = uart.handleMoveKeys()
+		
